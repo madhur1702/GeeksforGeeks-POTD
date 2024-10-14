c7ee323912a1aa9921eb7e6b4bb706c7fc5f614c
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-10-14 20:02:55 +0530

    Create Oct11_ReorganizeTheArray.cpp

---
+++ Oct11_ReorganizeTheArray.cpp
@@ -0,0 +1,21 @@
+
+
+class Solution {
+  public:
+
+vector<int> rearrange(const vector<int>& arr) {
+        // Code here
+        vector<int> ans(arr.size(),-1);
+        
+        
+        for(int i=0;i<arr.size();i++){
+            
+            if(arr[i] != -1){
+                ans[arr[i]] = arr[i];
+            }
+        }
+        
+        return ans;
+        
+    }
+};
