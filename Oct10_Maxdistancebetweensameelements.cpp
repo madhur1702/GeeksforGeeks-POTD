f0b68049202b601f9223dff4c8732f9a6524bf02
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-10-15 19:24:25 +0530

    Update Oct10_Maxdistancebetweensameelements.cpp

--- Oct10_Maxdistancebetweensameelements.cpp
+++ Oct10_Maxdistancebetweensameelements.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+  public:
+    
+int maxDistance(vector<int> &arr) {
+        unordered_map<int, int> memo;
+        int ans = -1; int n = arr.size();
+        for(int i = 0; i < n; i++){
+            (memo.find(arr[i]) == memo.end()) ? memo[arr[i]] = i + 1 : ans = max(ans, abs(i+1 - memo[arr[i]]));
+        }
+        return ans;
+    }
+    
+    
+};
